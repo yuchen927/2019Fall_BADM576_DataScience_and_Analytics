2e2a8dbe0d05c8fb313b99847db795ae341d6400
Jane Su <55065287+yuchen927@users.noreply.github.com>
2019-10-04 18:30:27 -0500

    Delete DrinkDeathDraft.R

--- Homework3/DrinkDeathDraft.R
+++
@@ -1,8 +0,0 @@
-
-library("readxl")
-DrinkDeath <- read_excel("C:/Users/Yu Chen Su/Documents/UIUC/2019 Fall/BADM 576 Data Science and Analytics/Homework/HW3/DrinkDeath.xlsx")
-
-#C:\Users\Yu Chen Su\Documents\UIUC\2019 Fall\BADM 576 Data Science and Analytics\Homework\HW3
-
-DrinkDeath
-
